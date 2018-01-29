f812f755d84785783a1b553ca8cd059ba9864490
stuminshull@gmail.com <stuminshull@gmail.com>
2018-01-28 17:58:16 -0800

    Add more FRIEND_TERMs

--- src/buddybot/__init__.py
+++ src/buddybot/__init__.py
@@ -1,11 +1,16 @@
 """
-Constants that define The Joke
-See: https://www.urbandictionary.com/define.php?term=I%27m%20not%20your%20friend%2C%20buddy
+All instances of The Joke are referring to the "I'm not your {buddy}, {pal}" meme from South Park
+See:
+
+https://www.urbandictionary.com/define.php?term=I%27m%20not%20your%20friend%2C%20buddy
+http://southpark.cc.com/clips/165191/im-not-your-friend-guy
+
+Set constants that define The Joke
 """
 
 # Templates to detect & print The Joke
 # Use raw strings for regex
-JOKE_REGEX_TEMPLATE = r"{opener} [a-z]+, [a-z]+."
+JOKE_REGEX_TEMPLATE = r"({opener}) ([a-zA-Z]+), ([a-zA-Z]+)."
 JOKE_PRINT_TEMPLATE = "{opener} {mirror_term}, {second_term}."
 
 # Support "Don't call me " variant
@@ -18,9 +23,12 @@ VALID_OPENERS = [
 FRIEND_TERMS = [
     'acquaintance',
     'associate',
+    'boss',
+    'boiii'
     'bro',
     'brother',
     'buddy',
+    'champ',
     'chum',
     'coach',
     'companion',
@@ -28,7 +36,9 @@ FRIEND_TERMS = [
     'comrade',
     'consort',
     'cousin',
+    'daddy',
     'deputy',
+    'fam',
     'friend',
     'friendo',
     'mate',
@@ -37,6 +47,9 @@ FRIEND_TERMS = [
     'peon',
     'ref',
     'sidekick',
+    'sister',
     'soulmate',
-    'temp'
+    'squad',
+    'temp',
+    'queen'
 ]
